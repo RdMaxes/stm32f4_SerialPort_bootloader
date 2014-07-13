058e95cac17de8d915db5109bdc188598237db13
RdMaxes <yc.huang0522@gmail.com>
2014-07-13 12:22:17 +0800

    template setup with usart2 function

---
+++ Hardware/LED/led.c
@@ -0,0 +1,44 @@
+#include <stm32f4xx.h>
+#include "led.h"
+ 
+//LED configuration 
+void LED_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	//LED GREEN
+	RCC_AHB1PeriphClockCmd(LED_GREEN_CLK,ENABLE);
+	GPIO_InitStruct.GPIO_Pin = LED_GREEN_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LED_GREEN_PORT, &GPIO_InitStruct);
+
+	//LED ORANGE
+	RCC_AHB1PeriphClockCmd(LED_ORANGE_CLK,ENABLE);
+	GPIO_InitStruct.GPIO_Pin = LED_ORANGE_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LED_ORANGE_PORT, &GPIO_InitStruct);
+
+	//LED RED
+	RCC_AHB1PeriphClockCmd(LED_RED_CLK,ENABLE);
+	GPIO_InitStruct.GPIO_Pin = LED_RED_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LED_RED_PORT, &GPIO_InitStruct);
+
+	//LED BLUE
+	RCC_AHB1PeriphClockCmd(LED_BLUE_CLK,ENABLE);
+	GPIO_InitStruct.GPIO_Pin = LED_BLUE_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LED_BLUE_PORT, &GPIO_InitStruct);		
+}
---
+++ Project/bootloader/main.c
@@ -0,0 +1,29 @@
+#include <stm32f4xx.h>
+#include "led.h"
+#include "usart2.h"
+#include "myprintf.h"
+
+int32_t test_var = 343422;
+
+static void delay(int32_t time)
+{
+	while(time--);
+}
+
+int main(void)
+{	
+	LED_Init();
+	Usart2_Init(9600);
+	Myprintf_Init(0x00,myputc);
+	
+	while(1) 
+	{
+		my_printf("Hello, variable value is %d\r\n",test_var);
+		delay(8000000);
+		LED_GREEN_ON();
+		delay(8000000);
+		LED_GREEN_OFF();		
+	}
+
+	return 0;
+}
