7eba23775e91da0da91ac9042847a7265e764047
RdMaxes <yc.huang0522@gmail.com>
2014-07-14 15:48:02 +0800

    delete old code

--- User_Lib/stmflash/stmflash.h
+++ User_Lib/stmflash/stmflash.h
@@ -7,19 +7,15 @@
 //	Sector [4]    Block Size = 64 KBytes
 //	Sector [5:11] Block Size = 128 KBytes
 //      ***Total: 1024 KBytes
-//Reference from ALIENTEK Warship Dev. Board IAP example
 //
 */ 
 
-//User Defined Area
-#define STM32_FLASH_SIZE 1024 	//STM32 Flash Size (KB) 		
-#define STM32_FLASH_WREN 1      //Enable Write       
+//User Defined Area      
 
 //FLASH Start Address
 #define STM32_FLASH_BASE 0x08000000 	//stm32 flash start address
 
-//Function Prototype 			
-extern void STMFLASH_Write(u32 WriteAddr,u16 *pBuffer,u16 NumToWrite);		 									   
+//Function Prototype 					 									   
 #endif
 
 
