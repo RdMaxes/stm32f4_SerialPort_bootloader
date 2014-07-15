5bd76ef1811fa25148db0823ba916bae020b6d43
RdMaxes <yc.huang0522@gmail.com>
2014-07-16 02:37:29 +0800

    delete old comment

--- User_Lib/stmflash/stmflash.c
+++ User_Lib/stmflash/stmflash.c
@@ -1,12 +1,4 @@
 #include "stmflash.h"
-/*
-//STM32F40x_41x Flash Memory Organization (User Manual PM0081):
-//	Sector [0:3]  Block Size = 16 KBytes
-//	Sector [4]    Block Size = 64 KBytes
-//	Sector [5:11] Block Size = 128 KBytes
-//      ***Total: 1024 KBytes
-//
-*/ 
 
 
 
