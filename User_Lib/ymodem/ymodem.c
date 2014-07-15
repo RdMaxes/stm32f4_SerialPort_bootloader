8f17a931ac09453ec341ada7abdd02841337c4f8
RdMaxes <yc.huang0522@gmail.com>
2014-07-16 00:47:45 +0800

    add file introduction

--- User_Lib/ymodem/ymodem.c
+++ User_Lib/ymodem/ymodem.c
@@ -1,29 +1,12 @@
-/**
-  ******************************************************************************
-  * @file    STM32F4xx_IAP/src/ymodem.c 
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    10-October-2011
-  * @brief   This file provides all the software functions related to the ymodem 
-  *          protocol.
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */
+/*
+//@brief :Data Tx/Rx via Ymodem protocol
+//        (*)STM32F40x_41x IAP flash is excuted in Ymodem_Receive() function
+//        (*)Modified from STM AN395 example
+//@Author:RdMaxes
+//@Data  :2014/07/15
+//@Note  :
+*/
 
-/** @addtogroup STM32F4xx_IAP
-  * @{
-  */ 
-  
 /* Includes ------------------------------------------------------------------*/
 #include "stmflash.h"
 #include "common.h"
