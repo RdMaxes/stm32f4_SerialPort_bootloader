fb31dc116552bced1cf26f272b3ffe9393f225d9
RdMaxes <yc.huang0522@gmail.com>
2014-07-16 16:48:34 +0800

    add application for flash part programming

---
+++ Project/app_flash/main.c
@@ -0,0 +1,28 @@
+#include <stm32f4xx.h>
+#include "led.h"
+#include "usart2.h"
+#include "myprintf.h"
+
+//Delay for a while
+//time: delay time
+static void delay(int32_t time)
+{
+	while(time--);
+}
+
+int main(void)
+{	
+	LED_Init();
+	Usart2_Init(230400);
+	Myprintf_Init(0x00,myputc);
+
+	while(1) 
+	{
+		
+		LED_GREEN_ON(); LED_BLUE_ON();LED_RED_ON();LED_ORANGE_ON(); delay(8000000);
+		
+		LED_GREEN_OFF(); LED_BLUE_OFF(); LED_RED_OFF(); LED_ORANGE_OFF(); delay(8000000);		
+	}
+
+	return 0;
+}
