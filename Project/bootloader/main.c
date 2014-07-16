5d7cb9de7758a60b592969ba332933f2757c21b5
RdMaxes <yc.huang0522@gmail.com>
2014-07-16 15:39:43 +0800

    test cmd switch part

--- Project/bootloader/main.c
+++ Project/bootloader/main.c
@@ -37,70 +37,71 @@ int main(void)
 	if (STM_FLASH_GetWriteProtectionStatus() == 0)   FlashProtection = 1;
 	else FlashProtection = 0;
 
-	//Show Main Menu
-	my_printf("\r\n======================================================================");
-	my_printf("\r\n========================= Main Menu ==================================");
-	my_printf("\r\nDownload Image To the STM32F4xx Internal Flash --------------------[1]");
-	my_printf("\r\nUpload Image From the STM32F4xx Internal Flash --------------------[2]");
-	my_printf("\r\nExecute The New Program -------------------------------------------[3]");
+	while(1) 
+	{
+		//Show Main Menu
+		my_printf("\r\n======================================================================");
+		my_printf("\r\n========================= Main Menu ==================================");
+		my_printf("\r\nDownload Image To the STM32F4xx Internal Flash --------------------[1]");
+		my_printf("\r\nUpload Image From the STM32F4xx Internal Flash --------------------[2]");
+		my_printf("\r\nExecute The New Program -------------------------------------------[3]");
 
-	if(FlashProtection != 0)
-	{
-	      my_printf("\r\nExecute The New Program -------------------------------------------[4]");
-	}
-	my_printf("\r\n======================================================================");	
-	my_printf("\r\nPlease select the option...");
-
-	//Receive a byte from usart2
-	cmd = Usart2_GetByte();
-	if (cmd == 0x31)
-	{
-	}
-	else if (cmd == 0x32)
-	{
-	}
-	else if (cmd == 0x33) /* execute the new program */
-	{
-//		JumpAddress = *(__IO uint32_t*) (APPLICATION_ADDRESS + 4);
-//		/* Jump to user application */
-//		Jump_To_Application = (pFunction) JumpAddress;
-//		/* Initialize user application's Stack Pointer */
-//		__set_MSP(*(__IO uint32_t*) APPLICATION_ADDRESS);
-//		Jump_To_Application();
-	}
-	else if ((cmd == 0x34) && (FlashProtection == 1))
-	{
-		/* Disable the write protection */
-		switch (STM_FLASH_DisableWriteProtection())
+		if(FlashProtection != 0)
 		{
-			case 1:
-			{
-				my_printf("\r\nWrite Protection disabled...");
-				FlashProtection = 0;
-				break;
-			}
-			case 2:
-			{
-				my_printf("\r\nError: Flash write unprotection failed...");
-				break;
-			}
-			default:{}
+		      my_printf("\r\nExecute The New Program -------------------------------------------[4]");
 		}
-	}
-	else
-	{
-		if (FlashProtection == 0)
+		my_printf("\r\n======================================================================");	
+		my_printf("\r\nPlease select the option...");
+
+		//Receive a byte from usart2
+		cmd = Usart2_GetByte();
+		if (cmd == 0x31)
 		{
-			my_printf("\r\nInvalid Number ! ==> The number should be either 1, 2 or 3");
+		
+		}
+		else if (cmd == 0x32)
+		{
+		}
+		else if (cmd == 0x33) /* execute the new program */
+		{
+	//		JumpAddress = *(__IO uint32_t*) (APPLICATION_ADDRESS + 4);
+	//		/* Jump to user application */
+	//		Jump_To_Application = (pFunction) JumpAddress;
+	//		/* Initialize user application's Stack Pointer */
+	//		__set_MSP(*(__IO uint32_t*) APPLICATION_ADDRESS);
+	//		Jump_To_Application();
+		}
+		else if ((cmd == 0x34) && (FlashProtection == 1))
+		{
+			/* Disable the write protection */
+			switch (STM_FLASH_DisableWriteProtection())
+			{
+				case 1:
+				{
+					my_printf("\r\nWrite Protection disabled...");
+					FlashProtection = 0;
+					break;
+				}
+				case 2:
+				{
+					my_printf("\r\nError: Flash write unprotection failed...");
+					break;
+				}
+				default:{}
+			}
 		}
 		else
 		{
-			my_printf("\r\nInvalid Number ! ==> The number should be either 1, 2, 3 or 4");
+			if (FlashProtection == 0)
+			{
+				my_printf("\r\nInvalid Number ! ==> The number should be either 1, 2 or 3");
+			}
+			else
+			{
+				my_printf("\r\nInvalid Number ! ==> The number should be either 1, 2, 3 or 4");
+			}
 		}
-	}
 
-	while(1) 
-	{
 		delay(8000000);
 		LED_GREEN_ON();
 		delay(8000000);
