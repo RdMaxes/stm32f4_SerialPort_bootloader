a1f48eac15dcff29e005490ed15f4292df5b5b69
RdMaxes <yc.huang0522@gmail.com>
2014-07-16 13:49:20 +0800

    edit the menuand command table

--- Project/bootloader/main.c
+++ Project/bootloader/main.c
@@ -12,12 +12,40 @@ static void delay(int32_t time)
 
 int main(void)
 {	
+	uint8_t FlashProtection = 0;	
+
 	LED_Init();
 	Usart2_Init(9600);
 	Myprintf_Init(0x00,myputc);
 	
+	//Show Program Information
+	my_printf("\r\n======================================================================");
+	my_printf("\r\n=          STM32F40x_41x In-Application Programming (IAP)            =");	
+	my_printf("\r\n=                        Author: RdMaxes                             =");
+	my_printf("\r\n=                        Version: 1.01                               =");
+	my_printf("\r\n======================================================================");
+	my_printf("\r\n\r\n");	
+	//Test if any sector of Flash memory where user application will be loaded is write protected
+	if (FLASH_If_GetWriteProtectionStatus() == 0)   
+	{
+		FlashProtection = 1;
+	}
+	else
+	{
+		FlashProtection = 0;
+	}
+	//Show Main Menu
+	my_printf("\r\n======================================================================");
+	my_printf("\r\n========================= Main Menu ==================================");
+	my_printf("\r\nDownload Image To the STM32F4xx Internal Flash --------------------[1]");
+	my_printf("\r\nUpload Image From the STM32F4xx Internal Flash --------------------[2]");
+	my_printf("\r\nExecute The New Program -------------------------------------------[3]");
 
-	
+	if(FlashProtection != 0)
+	{
+	      SerialPutString("\r\nExecute The New Program -------------------------------------------[4]");
+	}
+	my_printf("\r\n======================================================================");	
 
 	while(1) 
 	{
